f2a6b269ff0edd8ee1cd09209a5ca0a50bea8c5a
kara7z <kara.ossama12@gmail.com>
2026-01-13 10:32:30 +0100

    fist commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Point Twig to the templates folder
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+$twig = new Environment($loader);
+
+echo $twig->render('shop.twig', [
+    'title' => 'Loyalty Points System',
+    'user' => 'kara',
+    'points' => 120
+]);
